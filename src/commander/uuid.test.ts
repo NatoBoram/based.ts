c31e122b95d87ee1eb3c339a31f1414b49d8673f
Nato Boram <nato.boram+git@gmail.com>
2025-09-02 20:53:06 -0400

    ✅ Fix tests with suggestion from @coderabbitai

--- src/commander/uuid.test.ts
+++ src/commander/uuid.test.ts
@@ -5,52 +5,48 @@ import { uuid } from "./uuid.ts"
 
 describe("uuid", () => {
 	const console = vi.spyOn(globalThis.console, "log")
-	const getRandomBytesSpy = vi.spyOn(bytesModule, "getRandomBytes")
+	const getRandomBytes = vi.spyOn(bytesModule, "getRandomBytes")
 
-	afterEach(() => {
-		console.mockClear()
-		getRandomBytesSpy.mockClear()
-	})
-
-	afterAll(() => {
-		vi.restoreAllMocks()
-	})
+	afterEach(() => vi.clearAllMocks())
+	afterAll(() => vi.restoreAllMocks())
 
 	test("base 16", ({ expect }) => {
 		const bytes = new Uint8ClampedArray(16).fill(255)
-		getRandomBytesSpy.mockReturnValue(bytes)
+		getRandomBytes.mockReturnValue(bytes)
 
-		uuid({
-			base: "16",
-			space: base64Space,
-		})
+		uuid({ base: "16", space: base64Space })
 
+		expect(getRandomBytes).toHaveBeenCalledOnce()
+		expect(getRandomBytes).toHaveBeenCalledWith(16)
+
+		expect(console).toHaveBeenCalledOnce()
 		expect(console).toHaveBeenCalledWith("ffffffffffffffffffffffffffffffff")
 	})
 
 	test("base 64", ({ expect }) => {
-		const bytes = new Uint8ClampedArray([
-			0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
-		])
-		getRandomBytesSpy.mockReturnValue(bytes)
+		const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+		const bytes = new Uint8ClampedArray(array)
+		getRandomBytes.mockReturnValue(bytes)
 
-		uuid({
-			base: "64",
-			space: base64Space,
-		})
+		uuid({ base: "64", space: base64Space })
 
+		expect(getRandomBytes).toHaveBeenCalledOnce()
+		expect(getRandomBytes).toHaveBeenCalledWith(16)
+
+		expect(console).toHaveBeenCalledOnce()
 		expect(console).toHaveBeenCalledWith("g8310k61Mw92wIc3gUf")
 	})
 
 	test("base 2", ({ expect }) => {
 		const bytes = new Uint8ClampedArray([1, 0])
-		getRandomBytesSpy.mockReturnValue(bytes)
+		getRandomBytes.mockReturnValue(bytes)
 
-		uuid({
-			base: "2",
-			space: base64Space,
-		})
+		uuid({ base: "2", space: base64Space })
 
+		expect(getRandomBytes).toHaveBeenCalledOnce()
+		expect(getRandomBytes).toHaveBeenCalledWith(16)
+
+		expect(console).toHaveBeenCalledOnce()
 		expect(console).toHaveBeenCalledWith("100000000")
 	})
 })
